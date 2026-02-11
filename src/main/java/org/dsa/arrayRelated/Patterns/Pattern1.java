db65c8647dbba817e412bbda7e9128c918a88e99
Keshaw Krishna <keshawkrishna2007@gmail.com>
2026-02-11 21:27:42 +0530

    foe new

--- src/main/java/org/dsa/arrayRelated/Patterns/Pattern1.java
+++ src/main/java/org/dsa/arrayRelated/Patterns/Pattern1.java
@@ -15,6 +15,18 @@ public class Pattern1 {
         System.out.println("\n");
         pattern4(5);
 
+        System.out.println("\n");
+        pattern5(5);
+
+        System.out.println("\n");
+        pattern6(5);
+
+        System.out.println("\n");
+        pattern7(5);
+
+        System.out.println("\n");
+        pattern8(5);
+
     }
 
     /**
@@ -84,6 +96,84 @@ public class Pattern1 {
         for (int i = 0; i < number; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(i+1);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /**
+     * Pattern 5
+     *  *****
+     *  ****
+     *  ***
+     *  **
+     *  *
+    */
+
+    static void pattern5(int number){
+        for (int i = number; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /**
+     * Pattern 6
+     *  12345
+     *  1234
+     *  123
+     *  12
+     *  1
+    */
+
+    static void pattern6(int number){
+        for (int i = number; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /**
+     * Pattern 7
+          *
+         ***
+        *****
+       *******
+      *********
+    */
+
+    static void pattern7(int number){
+        for(int i = 0; i < number; i++){
+            for(int k = 0; k < number - i - 1; k++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i * 2 + 1; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /**
+     * Pattern 8
+      *********
+       *******
+        *****
+         ***
+          *
+    */
+
+    static void pattern8(int number){
+        for(int i = 0; i < number; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < (number * 2 - (i*2+1));k++){
+                System.out.print("*");
             }
             System.out.print("\n");
         }
